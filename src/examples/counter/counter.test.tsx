f3816b253493df6bde47dc4f100d0ee6b0a77d7d
Diana Janíčková <diana.janickova@heureka.cz>
2025-01-11 10:02:31 +0100

    feat: test counter component

--- src/examples/counter/counter.test.tsx
+++ src/examples/counter/counter.test.tsx
@@ -1,10 +1,18 @@
-import { screen, render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
 import Counter from '.';
+import { render } from './test/utilities.solution';
 
-test.todo('it should render the component', () => {});
+test('it should render the component', () => {
+  render(<Counter />);
+});
 
-test.todo(
-  'it should increment when the "Increment" button is pressed',
-  async () => {},
-);
+test('it should increment when the "Increment" button is pressed', async () => {
+  const { user } = render(<Counter />);
+  const count = screen.getByTestId('current-count');
+  expect(count).toHaveTextContent('0');
+  const button = screen.getByRole('button', { name: 'Increment' });
+
+  await user.click(button);
+
+  expect(count).toHaveTextContent('1');
+});
